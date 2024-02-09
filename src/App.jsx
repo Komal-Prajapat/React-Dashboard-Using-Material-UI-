a4c3378649d8e98be629c8bbd35405a231f618b5
Komal-Prajapat <komalprajapat267@gmail.com>
2024-02-09 19:46:02 +0530

    getup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Side_navbar from './Side_navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Side_navbar></Side_navbar>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Side_navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Side_navbar = () => {
+  return (
+    <div>
+      <h1>side navbar .jsx</h1>
+    </div>
+  )
+}
+
+export default Side_navbar
